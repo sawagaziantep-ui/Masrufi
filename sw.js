89cd9a636adf29f72399a85ad1c2c6ef44cbad31
sawagaziantep-ui <sawagaziantep@gmail.com>
2026-09-03 02:42:07 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "masarifi-cache-v2";
+const CACHE_NAME = "masarifi-cache-v3";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", (event) => {
   self.skipWaiting();
